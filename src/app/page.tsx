d75d7312f276c57f0abd01fb2fd437182ee8b446
“psst54” <psst5491@naver.com>
2024-10-14 20:59:18 +0900

    ✨ useNaverMap hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import useNaverMap from "@/hooks/useNaverMap";
+
 export default function Home() {
-  return <div>home</div>;
+  const naverMaps = useNaverMap();
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!naverMaps || !ref.current) {
+      return;
+    }
+
+    new naverMaps.Map(ref.current);
+  }, [naverMaps]);
+
+  if (!naverMaps) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div ref={ref} style={{ width: "500px", height: "500px" }}></div>;
+    </div>
+  );
 }
---
+++ src/hooks/useNaverMap.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useNaverMap() {
+  const [naverMaps, setNaverMaps] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.naver?.maps) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}&submodules=panorama,geocoder,drawing`;
+    script.async = true;
+
+    script.onload = () => {
+      setNaverMaps(window.naver.maps);
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return naverMaps;
+}
