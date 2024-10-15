9baec25e11265dd25f95c4605b8cc329160922bf
“psst54” <psst5491@naver.com>
2024-10-15 13:15:24 +0900

    💬 fix univ coords

--- src/constants/coords.ts
+++ src/constants/coords.ts
@@ -6,6 +6,6 @@ export interface Position {
 export const UNIV_COORDS = {
   EWHA: { lat: 37.5596817, lng: 126.9455855 },
   HONGIK: { lat: 37.5527434, lng: 126.9243838 },
-  SOGANG: { lat: 37.5512449, lng: 126.9379009 },
+  SOGANG: { lat: 37.5516266, lng: 126.9377722 },
   YONSEI: { lat: 37.5603201, lng: 126.9367637 },
 };
